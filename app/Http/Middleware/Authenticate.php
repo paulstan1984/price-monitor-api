f0d47784b675908a338c274c52d800a9b9adc2b6
Paul Stan <Paul Stan>
2021-01-06 22:38:02 +0200

    Update Authenticate.php

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Log;
 
 class Authenticate 
 {
     public function handle($request, $next)
     {
+        Log::debug('An informational message.');
+
         if($request->header('authorization') == 'asdasd') {
             return  $next($request);
         }
