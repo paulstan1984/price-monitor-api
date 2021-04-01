8b7fa79012f00998d80e158d3a4b8c55328a69a0
Paul Stan <Paul Stan>
2021-04-01 15:20:46 +0300

    Update Price.php

--- app/Price.php
+++ app/Price.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 class Price extends Model
 {
     protected $table = 'prices';
-    protected $primaryKey = 'id'; 
+    protected $primaryKey = 'id';
 
     public function product()
     {
@@ -20,28 +20,31 @@ class Price extends Model
         return $this->belongsTo(Store::class, 'store_id', 'id');
     }
 
-    public static function getLastPrice($product_id) {
+    public static function getLastPrice($product_id)
+    {
         $price = Price::where('product_id', $product_id)
             ->orderBy('created_at', 'DESC')
             ->first();
 
-        if(!empty($price)) {
+        if (!empty($price)) {
             return round($price->amount, 2);
         }
 
         return null;
     }
 
-    public static function getAvgPrice($product_id) {
+    public static function getAvgPrice($product_id)
+    {
         $avg_price = Price::where('product_id', $product_id)
             ->orderBy('created_at', 'DESC')
             ->avg('amount');
 
-        
+
         return round($avg_price, 2);
     }
 
-    public static function getStores($product_id) {
+    public static function getStores($product_id)
+    {
         $stores = Price::where('product_id', $product_id)
             ->join('stores', 'prices.store_id', '=', 'stores.id')
             ->select('stores.name')
@@ -49,54 +52,59 @@ class Price extends Model
             ->get();
 
         $store_names = [];
-        foreach($stores as $store){
-            $store_names[]=$store['name'];
+        foreach ($stores as $store) {
+            $store_names[] = $store['name'];
         }
-        
+
         return implode(', ', $store_names);
     }
 
 
-    public static function getStatistics($data) {
-        $query = Price::query(); 
+    public static function getStatistics($data, $groupByDates = true)
+    {
+        $query = Price::query();
 
-        $select_cols = [
-            DB::raw('DATE_FORMAT(prices.created_at, \'%Y-%m-%d\') as Date'),
+        $select_cols = [];
+        if ($groupByDates) {
+            $select_cols[] = DB::raw('DATE_FORMAT(prices.created_at, \'%Y-%m-%d\') as Date');
+        }
+
+        $select_cols = array_merge($select_cols, [
             DB::raw('ROUND(max(prices.amount), 2) as MaxPrice'),
             DB::raw('ROUND(avg(prices.amount), 2) as AvgPrice'),
             DB::raw('ROUND(sum(prices.amount), 2) as TotalPrice'),
-        ];
+        ]);
 
-        $group_by_cols = [
-            DB::raw('DATE_FORMAT(prices.created_at, \'%Y-%m-%d\')'),
-        ];
+        $group_by_cols = [];
+        if ($groupByDates) {
+            $group_by_cols[] = DB::raw('DATE_FORMAT(prices.created_at, \'%Y-%m-%d\')');
+        }
 
-        if(!empty($data['ProductsIds']) && is_array($data['ProductsIds'])){
+        if (!empty($data['ProductsIds']) && is_array($data['ProductsIds'])) {
             $query = $query->whereIn('product_id', $data['ProductsIds']);
-            
-            $select_cols []= 'products.id as ProductId';
-            $select_cols []= 'products.name as Product';
 
-            $group_by_cols []= 'products.id';
-            $group_by_cols []= 'products.name';
-            
+            $select_cols[] = 'products.id as ProductId';
+            $select_cols[] = 'products.name as Product';
+
+            $group_by_cols[] = 'products.id';
+            $group_by_cols[] = 'products.name';
         }
 
-        if(!empty($data['StoresIds']) && is_array($data['StoresIds'])){
+        if (!empty($data['StoresIds']) && is_array($data['StoresIds'])) {
             $query = $query->whereIn('store_id', $data['StoresIds']);
-            
-            $select_cols []= 'stores.id as StoreId';
-            $select_cols []= 'stores.name as Store';
-            
-            $group_by_cols []= 'stores.id';
-            $group_by_cols []= 'stores.name';
+
+            $select_cols[] = 'stores.id as StoreId';
+            $select_cols[] = 'stores.name as Store';
+
+            $group_by_cols[] = 'stores.id';
+            $group_by_cols[] = 'stores.name';
         }
 
-        if(!empty($data['StartDate'])){
+        if (!empty($data['StartDate'])) {
             $query = $query->where('prices.created_at', '>=', $data['StartDate']);
         }
 
-        if(!empty($data['EndDate'])){
+        if (!empty($data['EndDate'])) {
             $query = $query->where('prices.created_at', '<=', $data['EndDate']);
         }
 
@@ -104,9 +112,13 @@ class Price extends Model
             ->join('stores', 'prices.store_id', '=', 'stores.id')
             ->join('products', 'prices.product_id', '=', 'products.id')
             ->select($select_cols)
-            ->groupBy($group_by_cols)
-            ->orderBy(DB::raw('DATE_FORMAT(prices.created_at, \'%Y-%m-%d\')'))
-            ->get();
+            ->groupBy($group_by_cols);
+
+        if($groupByDates) {
+            $query = $query->orderBy(DB::raw('DATE_FORMAT(prices.created_at, \'%Y-%m-%d\')'));
+        }
+            
+        $query = $query->get();
 
         return $query;
     }
